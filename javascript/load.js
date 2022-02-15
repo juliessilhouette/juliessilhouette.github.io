69d96d92c7e063e54a7f4b92353355a16a8f1a8c
juliessilhouette <peachesrtastyx@gmail.com>
2022-02-15 10:20:39 +0700

    blog fixed

--- javascript/load.js
+++ javascript/load.js
@@ -1,3 +1,4 @@
+//DONOTDELETEIDIOTJULIAIKNOWWHATURTHINKINGRN
 function load(blog) {
     var i = 0
     do {
@@ -6,21 +7,20 @@ function load(blog) {
         div.setAttribute('id',i)
         document.getElementById('post').appendChild(div)
         //load toilet eject seat
-        var post = document.createElement('a')
-        post.setAttribute('href',blog.post[i].link)
-        post.setAttribute('id','link' + i)
-        document.getElementById(i).appendChild(post)
+        var link = document.createElement('a')
+        link.setAttribute('href',blog.post[i].link)
+        link.setAttribute('id','link' + i)
+        document.getElementById(i).appendChild(link)
         //load toilet
-        post = document.createElement('t')
-        post.innerHTML = blog.post[i].title
-        document.getElementById('link' + i).appendChild(post)
+        var title = document.createElement('t')
+        title.innerHTML = blog.post[i].title
+        document.getElementById('link' + i).appendChild(title)
         document.getElementById(i).appendChild(document.createElement('br'))
         //load paper
         var post = document.createElement('p')
         post.innerHTML = blog.post[i].text
         document.getElementById(i).appendChild(post)
         document.getElementById(i).appendChild(document.createElement('br'))
-        console.log(i)
 
         i = i + 1
     } while ( i < blog.post.length )
